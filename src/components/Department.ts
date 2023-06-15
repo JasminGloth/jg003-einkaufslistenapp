f8f06cffee079d2dade1518bc637c146d49dadc2
JasminGloth <jasmin.gloth@dci.education>
2023-06-15 21:26:00 +0200

    added price

--- src/components/Department.ts
+++ src/components/Department.ts
@@ -1,21 +1,24 @@
 interface IDepartment {
-    title:string;
+  title: string;
 }
 interface IItem {
-    title:string;
-    amount:number;
-    department:string;
+  title: string;
+  amount: number;
+  department: string;
+  price: number;
 }
 
-export const Department = (items:IItem[], department:IDepartment) => {
-    return /*html*/ `
+export const Department = (items: IItem[], department: IDepartment) => {
+  return /*html*/ `
     <h3>${department.title}</h3>
     <ul>
-		${items.filter(item => item.department === department.title)
+		${items
+      .filter((item) => item.department === department.title)
       .map((item) => {
-        return `<li> ${item.amount}x ${item.title}</li>`;
+        return `<li> ${item.amount}x ${item.title} ${(item.price =
+          item.amount * item.price)} €</li>`;
       })
       .join("")}
 	</ul>
     `;
-}
+};
