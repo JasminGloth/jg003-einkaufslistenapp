9dee552994d7a5df51afb82e2065acc470d471df
JasminGloth <jasmin.gloth@dci.education>
2023-06-15 14:30:55 +0200

    items on pageList

--- src/pages/PageList.ts
+++ src/pages/PageList.ts
@@ -1,7 +1,10 @@
+import items from '../data/items.json';
+
 export const PageList = () => {
 	return /*html*/`
-<div class="page pageWelcome">
-	<p>Welcome to this site.</p>
+<div class="page pageList">
+	<h2>Einkaufsliste</h2>
+	<p>Es gibt ${items.length} Items</p>
 </div>
 `; 
 }
